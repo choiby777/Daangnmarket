a6f8a1597f2a5db62ab8dae535ce2ef9e66e41a3
ByongYun Choi <choiby777@gmail.com>
2020-12-12 20:26:38 +0900

    Add Login Activity

--- app/src/main/java/com/daangnmarket/Home/HomeActivity.kt
+++ app/src/main/java/com/daangnmarket/Home/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.daangnmarket.Home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.daangnmarket.Login.LoginActivity
 import com.daangnmarket.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+//        val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+//        startActivity(intent)
     }
 }
