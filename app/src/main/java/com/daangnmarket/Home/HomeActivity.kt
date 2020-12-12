2a74510e1ce075014f5b819b94fa9dbff5089f4c
ByongYun Choi <choiby777@gmail.com>
2020-12-12 19:37:21 +0900

    Design Home Activity

---
+++ app/src/main/java/com/daangnmarket/Home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.daangnmarket.Home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.daangnmarket.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
