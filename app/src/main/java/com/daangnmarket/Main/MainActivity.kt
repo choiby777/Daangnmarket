021f8887d61b7acd80afe78b1c1469fcf5473d91
ByongYun Choi <choiby777@gmail.com>
2020-12-12 20:32:01 +0900

    rename home to main

--- app/src/main/java/com/daangnmarket/Main/MainActivity.kt
+++ app/src/main/java/com/daangnmarket/Main/MainActivity.kt
@@ -1,15 +1,13 @@
-package com.daangnmarket.Home
+package com.daangnmarket.Main
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.daangnmarket.Login.LoginActivity
 import com.daangnmarket.R
 
-class HomeActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
 
 //        val intent = Intent(this@HomeActivity, LoginActivity::class.java)
 //        startActivity(intent)
